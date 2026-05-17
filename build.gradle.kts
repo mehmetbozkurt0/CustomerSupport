bc8744c4d37ac8b1540f192f193640c7d0248bd1
Mehmet Bozkurt <mehmet.64.b@gmail.com>
2026-05-17 14:32:49 +0300

    Add SonarQube plugin and SonarCloud config

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,14 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.googleServices) apply false
+
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mehmetbozkurt0_CustomerSupport")
+        property("sonar.organization", "mehmetbozkurt0")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
